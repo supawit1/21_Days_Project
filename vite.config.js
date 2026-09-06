b1e998cebc731a8125dbaa4b20317ca2b081bbbe
PHONENI\Lenovo <carliwa.ch@gmail.com>
2026-09-06 22:04:31 +0700

    Update base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: '21_Days_Project', // ใส่ชื่อ Repository บน GitHub ที่ตั้งไว้
+})
